2f7cea9a86e3911b5d0e6035985502736bb5123e
hakira <jsumbalan@gmail.com>
2023-02-10 22:06:34 +0800

    Update 1_📷_Timelapse.py

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 
 @st.cache(persist=True)
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
-    geemap.ee_initialize(auth_mode='gcloud')
+    geemap.ee_initialize(token_name=token_name)
 
 
 st.sidebar.title("About")
